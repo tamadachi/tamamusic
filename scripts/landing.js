3ae4b275b7544118148563656ea0bd96034b536e
ckrgz-002 <krcmisc@gmail.com>
2017-07-18 21:08:48 -0400

    forEach

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,21 @@
 var pointsArray = document.getElementsByClassName('point'); 
+//checkpoint 9 Assignment forEach:
+//forEach will use the array named pointsArray, but what will the callback function be?
+// what do I want to happen with each element in the array? Answer - revealPoint! So revealPoint will be
+//the callback function.
+//Pull revealPoint out of animatePoints function, add the forEach function to the body of animate points
+ var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 var animatePoints = function(points) {
-                      
-                   var revealPoint = function(index) {
-                     points[index].style.opacity = 1;
-                     points[index].style.transform = "scaleX(1) translateY(0)";
-                     points[index].style.msTransform = "scaleX(1) translateY(0)";
-                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 }
-                    for (var i = 0; i < points.length; i++){
-                 revealPoint(i);
-                    }
-              };
+    forEach(points, revealPoint); //points, in this case is a parameter of the animatePoints function
+    //when animatePoints is called, the array pointsArray is passed in, which will be passed in the forEach function
+
+     };
              
 
 window.onload = function() {
